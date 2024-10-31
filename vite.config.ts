1dc8a53f0a156f78eeb5e4bc2d0171a5d382eebd
Tobias <tobias.loveridge@gmail.com>
2024-10-31 14:46:12 +0000

    Updated vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/logistics-uk-fullstack-exercise/', 
+  plugins: [react()], 
+  build: { 
+    outDir: 'dist', 
+    assetsDir: 'assets', 
+  },
 })
