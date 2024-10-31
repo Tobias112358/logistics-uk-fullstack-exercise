9cd1eea961bea0fa0e48580861f4ec320aaddf64
Tobias112358 <Tobias112358@users.noreply.github.com>
2024-10-31 14:39:44 +0000

    deploy: fcc037fd2fa92345a019413a8b855be0e79d71bd

--- src/components/Holding/Holding.types.ts
+++
@@ -1,3 +0,0 @@
-export interface HoldingProps {
-    title: string
-}
--- src/components/components.mod.ts
+++
@@ -1,5 +0,0 @@
-//A module file that exports the header, sidebar and body components
-export { Header } from './Header/Header.tsx'
-export { Sidebar } from './Sidebar/Sidebar.tsx'
-export { Body } from './Body/Body.tsx'
-export { Holding } from './Holding/Holding.tsx'
