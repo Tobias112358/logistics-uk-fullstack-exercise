475c6be7ce88d0cd99f864529b5cdb021f8468f1
Tobias <tobias.loveridge@gmail.com>
2024-11-03 16:26:37 +0000

    Added base path for github page.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()], 
+  base: '/logistics-uk-fullstack-exercise/',
   build: { 
     outDir: 'dist', 
     assetsDir: 'assets', 
